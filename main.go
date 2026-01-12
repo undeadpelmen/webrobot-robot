cd8323b73172708e5778e0bd6e28131873564aad
undead <undead@comp.comp>
2026-01-12 15:16:52 +0300

    Base Crawler control corutine

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package webrobotrobot
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("hi")
+
+}
+
+type Robot interface {
+	SetSpeed(int) error
+	Forward() error
+	Stop() error
+	Backward() error
+	Left() error
+	Right() error
+}
+
+func RobotControlFunc(cmd chan string, errch chan error, robot Robot) {
+
 }
